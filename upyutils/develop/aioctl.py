90316c3d0ce1e444c6a48f1fcd3235e20ef6de7d
Carlosgg <carlosgilglez@gmail.com>
2023-01-27 05:53:16 +0000

    aiotools improvement

--- upyutils/develop/aioctl.py
+++ upyutils/develop/aioctl.py
@@ -2,31 +2,51 @@
 
 import uasyncio as asyncio
 import sys
+import time
+
+_SCHEDULE = False
+try:
+    import aioschedule
+
+    _SCHEDULE = True
+except Exception:
+    pass
 
 _AIOCTL_GROUP = None
 _AIOCTL_LOG = None
+_DEBUG = False
 
 
 def aiotask(f):
+    # print(f.__name__)
+
     async def _task(*args, **kwargs):
         on_error = kwargs.get("on_error")
         on_stop = kwargs.get("on_stop")
-        _id = kwargs.get("id")
+        _id = kwargs.get("_id")
         if on_error:
             kwargs.pop("on_error")
         if on_stop:
             kwargs.pop("on_stop")
         if _id:
-            kwargs.pop("id")
+            kwargs.pop("_id")
+        else:
+            _id = f.__name__
         try:
             result = await f(*args, **kwargs)
+            if _id in group().tasks.keys():
+                group().tasks[_id].done_at = time.time()
             return result
         except asyncio.CancelledError:
+            if _id in group().tasks.keys():
+                group().tasks[_id].done_at = time.time()
             if callable(on_stop):
                 return on_stop()
             return on_stop
 
         except Exception as e:
+            if _id in group().tasks.keys():
+                group().tasks[_id].done_at = time.time()
             log = kwargs.get("log")
             if log:
                 log.error(f"[{_id}]" + f" {e.__class__.__name__}: {e.errno}")
@@ -34,15 +54,33 @@ def aiotask(f):
                 return on_error(e)
             return e
 
-    return _task
+    return _task, f.__name__
 
 
 def create_task(coro, *args, **kwargs):
-    name = kwargs.get("name", coro.__name__)
+    name = None
+    if isinstance(coro, follow.__class__):
+        name = kwargs.get("name", coro.__name__)
     if "name" in kwargs:
-        kwargs.pop("name")
+        name = kwargs.pop("name")
+    # print(type(coro), coro)
 
-    task = asyncio.create_task(coro(*args, **kwargs))
+    # print(dir(coro))
+    if isinstance(coro, tuple):
+        coro, _name = coro[0], coro[1]
+        if not name:
+            name = _name
+    if callable(coro):
+        _coro = coro(*args, **kwargs)
+        if isinstance(_coro, tuple):
+            _coro, _name = _coro[0], _coro[1]
+            if not name:
+                name = _name
+    # print(type(_coro), _coro)
+    task = asyncio.create_task(_coro)
+    # print(task, coro, name)
+    # print(dir(task))
+    # print(dir(task.coro))
     return Taskctl(coro, task, name, args, kwargs)
 
 
@@ -53,6 +91,8 @@ class Taskctl:
         self.name = name
         self.args = args
         self.kwargs = kwargs
+        self.since = time.time()
+        self.done_at = None
 
 
 class TaskGroup:
@@ -108,14 +148,24 @@ def delete(*args):
             _AIOCTL_GROUP.tasks.pop(name)
 
 
-def status(name=None, log=True):
-    global _AIOCTL_GROUP, _AIOCTL_LOG
+def status(name=None, log=True, debug=False):
+    global _AIOCTL_GROUP, _AIOCTL_LOG, _SCHEDULE, _DEBUG
+    if _DEBUG:
+        debug = _DEBUG
     if not name:
-        return status_all(log=log)
+        return status_all(log=log, debug=debug)
     if name in _AIOCTL_GROUP.tasks:
         if _AIOCTL_GROUP.tasks[name].task.done():
             _AIOCTL_GROUP.results[name] = _AIOCTL_GROUP.tasks[name].task.data
             status = "done"
+            _done_at = group().tasks[name].done_at
+            if _SCHEDULE:
+                if _done_at:
+                    _done_at = time.localtime(_done_at)
+                    _done_at = aioschedule.get_datetime(_done_at)
+                    _done_delta = time.time() - group().tasks[name].done_at
+                    _done_at += f"; {aioschedule.tmdelta_fmt(_done_delta)} ago"
+
             data = _AIOCTL_GROUP.results[name]
             if issubclass(data.value.__class__, Exception):
                 status = "\u001b[31;1mERROR\u001b[0m"
@@ -124,13 +174,29 @@ def status(name=None, log=True):
                     + f" {data.value.value}"
                 )
 
-            print(f"{name}: status: {status} --> result: " + f"{data}")
+            print(f"{name}: status: {status} @ {_done_at} --> result: " + f"{data}")
+            if debug:
+                c_task = _AIOCTL_GROUP.tasks[name]
+                print(f"    ┗━► args: {c_task.args}, kwargs: {c_task.kwargs}")
+            if _SCHEDULE:
+                aioschedule.status_sc(name, debug=debug)
             if log and _AIOCTL_LOG:
                 _AIOCTL_LOG.cat(grep=name)
                 print("<" + "-" * 80 + ">")
         else:
+            # 2023-01-12 04:42:51 GMT; 2 weeks 0 days ago
+            _since_str = time.localtime(group().tasks[name].since)
+            _since_delta = time.time() - group().tasks[name].since
 
-            print(f"{name}: status: \033[92mrunning\x1b[0m")
+            if _SCHEDULE:
+                _since_str = aioschedule.get_datetime(_since_str)
+                _since_str += f"; {aioschedule.tmdelta_fmt(_since_delta)}"
+            print(f"{name}: status: \033[92mrunning\x1b[0m since {_since_str} ago")
+            if debug:
+                c_task = _AIOCTL_GROUP.tasks[name]
+                print(f"    ┗━► args: {c_task.args}, kwargs: {c_task.kwargs}")
+            if _SCHEDULE:
+                aioschedule.status_sc(name, debug=debug)
             if log and _AIOCTL_LOG:
                 _AIOCTL_LOG.cat(grep=name)
                 print("<" + "-" * 80 + ">")
@@ -150,6 +216,12 @@ def result(name=None):
         return
 
 
+def debug():
+    global _DEBUG
+    _DEBUG = not _DEBUG
+    print(f"debug mode: {_DEBUG}")
+
+
 def result_all(as_dict=False):
     global _AIOCTL_GROUP
     if not as_dict:
@@ -159,10 +231,11 @@ def result_all(as_dict=False):
         return {name: result(name) for name in _AIOCTL_GROUP.tasks.keys()}
 
 
-def status_all(log=True):
+def status_all(log=True, debug=False):
     global _AIOCTL_GROUP
     for name in _AIOCTL_GROUP.tasks.keys():
-        status(name, log=log)
+        if name:
+            status(name, log=log, debug=debug)
 
 
 def start(name):
